182da8f4252467ecd59aa44978425bc8672cd903
baby神 <455416841@qq.com>
2017-07-24 13:35:30 +0800

    Update qq机器人.py

--- qq机器人.py
+++ qq机器人.py
@@ -5,7 +5,8 @@ import requests
 import qqbot
 import json
 
-mess = json.load(open('qq.txt','r'))
+mess = json.load(open('qq.txt', 'r'))
+
 
 def weather(city):
     r = ""
@@ -26,20 +27,17 @@ def onQQMessage(bot, contact, member, content):
     if "@ME" in content:
         con = '圈我干啥？？'
         bot.SendTo(contact, con)
-    elif "hello" in content:
-        con = "你好啊！"
-        bot.SendTo(contact, con)
-    elif "#study#" in content and len(content.split()) >= 3:
+    elif content.startswith("#study#") and '#get#' not in content and len(content.split()) >= 3:
         key, *value = content.split()[1:]
         mess[key] = value
         con = "录入成功！"
         json.dump(mess, open('qq.txt', 'w'))
         bot.SendTo(contact, con)
-    elif "#get#" in content and len(content.split()) >= 2:
+    elif content.startswith('#get#') and len(content.split()) >= 2:
         key = content.split()[1]
         con = mess.get(key) if key in mess else '不存在'
         bot.SendTo(contact, ' '.join(con))
-    elif "#weather#" in content and len(content.split()) >= 2:
+    elif content.startswith("#weather#") and len(content.split()) >= 2:
         con = weather(content.split()[1])
         bot.SendTo(contact, con)
 
