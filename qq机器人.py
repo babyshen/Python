0a5ac68528164599ea2639e48b44401743ca2214
baby神 <455416841@qq.com>
2017-07-25 17:35:57 +0800

    Update qq机器人.py

--- qq机器人.py
+++ qq机器人.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# 可以查询天气预报，进行学习录入
 
+import random
+import hashlib
 import requests
 import qqbot
 import json
@@ -11,15 +13,37 @@ mess = json.load(open('qq.txt', 'r'))
 def weather(city):
     r = city
     url = 'http://www.sojson.com/open/api/weather/json.shtml?city=' + city
+    
     html = requests.get(url)
     result = json.loads(html.text)
     if result.get('status') != 200:
-        return "暂无【%s】天气预报！" %city
+        return "暂无【%s】天气预报！" % city
     res = result.get('data').get('forecast')
     for i in res:
         r += '\r\n' + i.get('date') + ' ' + i.get('type') + ' ' + i.get('low') + ' ' + i.get('high') + ' ' + i.get(
             'fengli') + ' ' + i.get('fengxiang')
     return r
+
+
+def translate(word):
+    appid = 'xxoo'
+    secretKey = 'ooxx'
+
+    myurl = 'http://api.fanyi.baidu.com/api/trans/vip/translate'
+    fromLang = 'auto'
+    toLang = 'auto'
+    salt = random.randint(32768, 65536)
+
+    sign = appid + word + str(salt) + secretKey
+    m1 = hashlib.md5()
+    m1.update(sign.encode())
+    sign = m1.hexdigest()
+    myurl = myurl + '?appid=' + appid + '&q=' + word + '&from=' + fromLang + '&to=' + toLang + '&salt=' + str(
+        salt) + '&sign=' + sign
+
+    r = requests.get(myurl).text
+    res = json.loads(r)
+    return res.get('trans_result')[0].get('dst')
 
 
 @qqbot.QQBotSlot
@@ -39,6 +63,10 @@ def onQQMessage(bot, contact, member, content):
         bot.SendTo(contact, ' '.join(con))
     elif content.startswith("#weather#") and len(content.split()) >= 2:
         con = weather(content.split()[1])
+        bot.SendTo(contact, con)
+    elif content.startswith('#translate#') and len(content.split()) >= 2:
+        word = ' '.join(content.split()[1:])
+        con = translate(word)
         bot.SendTo(contact, con)
 
 
