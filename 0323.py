0525243922dd4ee474a7b72733848df5c62a5ef7
baby神 <455416841@qq.com>
2018-03-23 16:47:18 +0800

    Update 0323.py

--- 0323.py
+++ 0323.py
@@ -3,7 +3,6 @@
 # psutil 为第三方模块
 
 from ctypes import *
-import subprocess
 import win32api
 import time
 import errno
@@ -31,30 +30,34 @@ def document_record(doc):  # 记录格式化的时间，U盘卷名，耗时
     with open("USB记录.txt", "a", encoding="utf-8") as f:
         f.write(doc + "\n")
 
-def file_fill(drive_file):
+def file_fill(drive_file): # 填充U盘
     write_str = "!" * 1024 * 1024 * 5  # 5MB
-    with open(drive_file, "a") as f:
-        while True:
-            try:
-                f.write(write_str)
-                f.flush()
-            except Exception :
-                break
+    try:
+        with open(drive_file, "a") as f:
+            while True:
+                try:
+                    f.write(write_str)
+                    f.flush()
+                except IOError as err:
+                    if err.errno == errno.ENOSPC:
+                        write_str_len = len(write_str)
+                        if write_str_len > 1: # 填充满为止
+                            write_str = write_str[0:(int(write_str_len/2))]
+                        else:
+                            break
+    except OSError :
+        pass
 
 def main(): # 主函数
     for label in drire_label():
         start_time = time.time()  # 开始时间
         now_time = time.strftime("%F %T", time.localtime())  # 格式化时间
         volume_name, _, _, _, volume_filesystem = win32api.GetVolumeInformation(label)  # 获取卷名和文件系统
+        
         format_drive(label, "NTFS", volume_name)  # 第一次格式化
-
-        free_space = psutil.disk_usage(label).free # 获取磁盘可用空间
-        # subprocess.Popen("fsutil file createnew " + label + "\\test.tmp " + str(free_space),
-        #                            shell=True,
-        #                            stderr=subprocess.PIPE,
-        #                            stdout=subprocess.PIPE).communicate() # 填充磁盘
-        # file_fill(label + "\\test.tmp")
+        file_fill(label + "\\test.tmp") # 填充U盘
         format_drive(label, volume_filesystem, volume_name)  # 第二次格式化
+        
         end_time = time.time()  # 结束时间
         log_info =  "%s %s 格式化完成，耗时 %.2f 秒。" % (now_time, volume_name, end_time - start_time)
         document_record(log_info)
